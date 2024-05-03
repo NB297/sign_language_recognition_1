bc5567d38dfc45ed2fed55acfe9e0da2f1731dda
NB297 <79568681+NB297@users.noreply.github.com>
2024-05-04 01:24:10 +0530

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import tensorflow as tf
-
-def main():
-    # Display the TensorFlow version
-    st.write("TensorFlow version:", tf.__version__)
-
-if __name__ == "__main__":
-    main()
